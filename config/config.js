c93aa10f9bd43f7f3201f4b3f52af47d16e6db2e
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-11-16 16:00:43 +0530

    connect to supabase

---
+++ config/config.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_USERNAME ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_NAME
+) {
+  throw new Error(
+    "Please set all DB environment variables in .env file!!!!!!!!!!!!!!!!!"
+  );
+}
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
